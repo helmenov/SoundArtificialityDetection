27bc2a06275859a0205e83279cd72ed657ee9bb7
Kotaro SONODA <kotaro1976@gmail.com>
2022-11-30 18:13:37 +0900

    v0.1.0

--- README.md
+++
@@ -1,9 +0,0 @@
-# SoundArtificialityDetection
-人工音と天然音とを判別する．
-
-- 規則性/整然性:人工>天然？
-
-
-
-
-
---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['soundartificialitydetection']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SoundFile>=0.10.3,<0.11.0',
+ 'japanize-matplotlib>=1.1.3,<2.0.0',
+ 'matplotlib>=3.5.1,<4.0.0',
+ 'numpy>=1.22.3,<2.0.0',
+ 'scipy>=1.8.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'soundartificialitydetection',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Kotaro SONODA',
+    'author_email': 'kotaro1976@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<3.11',
+}
+
+
+setup(**setup_kwargs)
