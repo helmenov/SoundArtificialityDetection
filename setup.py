8d1dba621ed52c71430234c28af17f027253dfe5
Kotaro SONODA <kotaro1976@gmail.com>
2022-12-01 17:26:32 +0900

    v0.2.0

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ packages = \
 ['soundartificialitydetection']
 
 package_data = \
-{'': ['*']}
+{'': ['*'], 'soundartificialitydetection': ['ASVspoof2019Contrib/*']}
 
 install_requires = \
 ['SoundFile>=0.10.3,<0.11.0',
  'japanize-matplotlib>=1.1.3,<2.0.0',
+ 'librosa>=0.9.2,<0.10.0',
  'matplotlib>=3.5.1,<4.0.0',
  'numpy>=1.22.3,<2.0.0',
+ 'pandas>=1.5.2,<2.0.0',
  'scipy>=1.8.0,<2.0.0']
 
 setup_kwargs = {
     'name': 'soundartificialitydetection',
-    'version': '0.1.0',
+    'version': '0.2.0',
     'description': '',
     'long_description': 'None',
     'author': 'Kotaro SONODA',
